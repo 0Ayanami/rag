185cf8464556d53dcb4c784149a483ec4ea77c1f
Ayanami <mkayanami@gmail.com>
2026-03-24 19:45:40 +0800

    增加了历史会话功能

--- KnowledgeBase/app_file_uploader.py
+++ KnowledgeBase/app_file_uploader.py
@@ -1,11 +1,17 @@
 """
 使用streamlit实现在线上传文件功能
+streamlit run app_file_uploader.py
 """
 import streamlit as st
+from knowledgebase import KnowledgeBaseService
+import time
 
 st.title("文件上传服务")
 
 uploader_file = st.file_uploader("上传文件", type=["pdf", "docx", "txt"], accept_multiple_files=False)
+
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBaseService()
 
 if uploader_file is not None:
     file_name = uploader_file.name
@@ -16,4 +22,9 @@ if uploader_file is not None:
     st.write(f"文件大小: {file_size:.2f} kb | 文件类型: {file_type}")
 
     text = uploader_file.getvalue().decode("utf-8")
-    st.text_area("文件内容预览", text, height=300)
+    st.text_area("文件内容预览", text[:200], height=300)
+
+    with st.spinner("正在载入知识库..."):
+        time.sleep(1)
+        result = st.session_state["service"].upload_by_string(text, file_name)
+        st.success(result)
--- KnowledgeBase/config_data.py
+++ KnowledgeBase/config_data.py
@@ -11,3 +11,12 @@ chunk_size = 1000
 chunk_overlap = 100
 separators = ["\n\n", "\n", "。", ".", "!", "！", "?", "？", " ", ""]    
 max_split_char_number = 1000
+
+# 检索器配置
+similarity_threshold = 1
+
+# embedding模型配置
+embed_model_name = "qwen3-embedding:0.6b"
+
+# chat模型配置
+chat_model_name = "qwen3:4b"
